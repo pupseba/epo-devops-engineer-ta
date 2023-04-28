c63ee9cd9f75874fe7e38b23f910d6d5af9530c7
Sebastián Greco <38133294+pupseba@users.noreply.github.com>
2023-04-28 19:15:33 +0200

    Change base image to something more standard

--- tf-provision-vms/masters.tf
+++ tf-provision-vms/masters.tf
@@ -1,18 +1,19 @@
 resource "google_compute_instance" "master" {
-  count        = 3
+  count        = 1
   name         = format("master%02d", count.index + 1)
   machine_type = "e2-micro"
   zone         = "europe-west6-a"
 
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-101-lts"
+      image = "debian-cloud/debian-11"
       size = "20"
     }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.main.name
+    subnetwork = google_compute_subnetwork.private.name
   }
 
   metadata = {
