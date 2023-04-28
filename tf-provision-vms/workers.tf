7ece89173886a2915829b08ef6ae7f9afe1337d4
Sebastián Greco <38133294+pupseba@users.noreply.github.com>
2023-04-28 17:39:14 +0200

    Fix typo

--- tf-provision-vms/workers.tf
+++ tf-provision-vms/workers.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "worker" {
   count        = 3
-  name         = "format("worker%02d", count.index + 1)"
+  name         = format("worker%02d", count.index + 1)
   machine_type = "e2-micro"
   zone         = "europe-west6-a"
 
