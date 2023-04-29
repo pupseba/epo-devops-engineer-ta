ecacb6b90fe365306119557635ec99dbb2397450
Sebastián Greco <38133294+pupseba@users.noreply.github.com>
2023-04-29 03:50:34 +0200

    explicit kubeconfig for calico install

--- tf-provision-vms/scripts/startup.sh
+++ tf-provision-vms/scripts/startup.sh
@@ -55,12 +55,12 @@ systemctl restart containerd ; systemctl enable containerd
 # Executes code depending on the node it is running on
 if [[ $HOSTNAME == "master01" ]]; then
   kubeadm init --control-plane-endpoint=master01 --pod-network-cidr=192.168.0.0/16 &> /tmp/kubeadm_output
-  sleep 180
+  sleep 90
   mkdir -p /root/.kube
   cp -i /etc/kubernetes/admin.conf /root/.kube/config
   chown $(id -u):$(id -g) /root/.kube/config
   while [[ $(kubectl get cs | grep Healthy | wc -l) -ne 3 ]]; do sleep 10; done
-  kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml
+  kubectl --kubeconfig=/root/.kube/config apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml
 elif [[ $HOSTNAME == master* ]]; then
   if [[ $HOSTNAME != "master01" ]]; then
   sleep 180
